839bfb72bfc92e35429454496fc1a39db2d47742
Jordy Neefs <git@jordyneefs.be>
2023-11-29 13:31:20 +0100

    Update

---
+++ app/src/main/java/com/example/weatherapp/view/ScheduleListFragment.kt
@@ -0,0 +1,30 @@
+package com.example.weatherapp.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.weatherapp.R
+
+class ScheduleListFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_schedule_list, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            ScheduleListFragment().apply {
+            }
+    }
+}
